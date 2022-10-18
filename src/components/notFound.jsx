ec4c503daa8848a531ecc53a78097e44437238ab
atlasgeeksrb <rb@atlasgeekservices.com>
2022-10-18 11:43:09 -0400

    Stub components for not found page and payment complete

---
+++ src/components/notFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class NotFound extends Component {
+    render() { 
+        return (
+            <React.Fragment>
+                <h1>Not Found</h1>
+                <p>You step in the water but the stream has moved on. This page is not here.</p>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default NotFound;
---
+++ src/components/paymentComplete.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+class PaymentComplete extends Component {
+    state = {  } 
+    render() { 
+        return ("Your payment is complete!");
+    }
+}
+ 
+export default PaymentComplete;
