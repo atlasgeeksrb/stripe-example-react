487fb0b9ee272a8d2ac7699aa07533f8229aa07c
atlasgeeksrb <rb@atlasgeekservices.com>
2022-10-18 16:37:00 -0400

    Create placeholder payment complete page
    Simple status display

--- src/components/paymentComplete.jsx
+++ src/components/paymentComplete.jsx
@@ -1,8 +1,29 @@
 import { Component } from 'react';
+import {getPaymentDetails} from '../services/stripePaymentService';
 class PaymentComplete extends Component {
-    state = {  } 
+    state = {  
+        paymentInfo: null,
+    } 
+
+    async componentDidMount() {
+        const pmtId = new URLSearchParams(window.location.search).get(
+            "payment_intent"
+        );
+        try {
+            const deets = await getPaymentDetails(pmtId);
+            this.setState({paymentInfo:deets.data});            
+        } catch(ex) {
+            console.log(ex);
+        }
+    }
+
     render() { 
-        return ("Your payment is complete!");
+        const {paymentInfo} = this.state;
+        console.log(paymentInfo);
+        const status = paymentInfo && paymentInfo.status ? `(${paymentInfo.status})` : "";
+        return (
+            `Your payment is complete! ${status}`
+        );
     }
 }
  
